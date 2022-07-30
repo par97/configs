8d91874e66ef67a3aaf43af0b5cbeb5b593bd38c
par97 <102787934+par97@users.noreply.github.com>
2022-07-30 22:58:41 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,40 @@
 #!/usr/bin/env bash
-set -eu 
+set -u 
 
-echo "Please install curl, zsh, and oh-my-zsh first"
-echo 'Note: oh-my-zsh could be installed by: sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"'
+if ! [ -f "$(which curl)" ]; then 
+    echo "Error: curl is not installed, please install it first"
+    exit
+fi
+
+if ! [ -f "$(which zsh)" ]; then
+    echo "Error: zsh is not installed, please install it first"
+    exit
+fi 
+
+if ! [ -f ~/.oh-my-zsh/oh-my-zsh.sh ]; then
+    echo "oh-my-zsh is not installed, install it now"
+    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi 
+
 echo ""
-echo -n "continue? [y/N]: "
+echo -n "start to install? [y/N]: "
 read -r input
 if [[ $input != "y" ]]; then
     echo "exit"
     exit 0
 fi
 
+echo "set up vim"
 cp vim/.vimrc ~/.vimrc
 cp -R vim/.vim ~/
 
+echo "set up oh-my-zsh custom theme and plugin"
 cp -R ohmyzsh-custom/custom ~/.oh-my-zsh/
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+echo "add custom settings to .zshrc"
 cat zsh/zshrc >> ~/.zshrc
+
+echo "done"
